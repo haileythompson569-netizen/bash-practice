cd5718542f060747dbabf6e189befde11f428f85
Hailey Thompson <haileythompson569@gmail.com>
2026-06-14 19:44:55 -0400

    Add countdown and stopwatch scripts

---
+++ countdown.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Countdown script
+COUNTDOWN=$1
+while [ $COUNTDOWN -gt 0 ]; do
+        echo $COUNTDOWN
+        sleep 1
+        COUNTDOWN=$((COUNTDOWN - 1))
+    done
+    echo "Countdown complete!"
