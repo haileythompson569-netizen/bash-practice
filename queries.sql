572e6c8a2b86dec7213b8256796e595a02cd5dce
Hailey Thompson <haileythompson569@gmail.com>
2026-06-15 13:30:37 -0400

    Add SQL schema and practice queries

---
+++ queries.sql
@@ -0,0 +1,10 @@
+
+SELECT * FROM users;
+
+SELECT * FROM users WHERE city = 'Houston';
+
+SELECT name, age FROM users WHERE age > 35;
+
+SELECT * FROM users ORDER BY age DESC;
+
+
---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE logins (
+    id INTEGER,
+    name TEXT,
+    age INTEGER,
+    city TEXT
+);
+
+INSERT INTO logins(id, name, age, city) VALUES
+(1, 'Susan', 38, 'New York'),
+(2, 'Bill', 50, 'Los Angeles'),
+(3, 'Caroline', 45, 'Chicago'),
+(4, 'Greyson', 34, 'Houston'),
+(5, 'Ella', 22, 'Phoenix');  
